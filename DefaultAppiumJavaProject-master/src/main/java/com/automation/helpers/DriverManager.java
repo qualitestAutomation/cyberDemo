235dc1809e945c6965eca12424ec23291b20d876
Shahar Nakash <shahar.nakash@gmail.co.il>
2019-07-01 23:06:19 +0300

    try add access whitelist

--- DefaultAppiumJavaProject-master/src/main/java/com/automation/helpers/DriverManager.java
+++ DefaultAppiumJavaProject-master/src/main/java/com/automation/helpers/DriverManager.java
@@ -235,6 +235,10 @@ public class DriverManager {
 
     private void setSelenium() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/" + (System.getProperty("os.name").toLowerCase().contains("mac") ? "chromedriver_2" : "chromedriver"));
+        capabilities.setCapability("network.proxy.http", "82.80.139.21");
+        capabilities.setCapability("network.proxy.http_port", "7070");
+        capabilities.setCapability("webdriver.chrome.args", Arrays.asList("--verbose --whitelisted-ips=''"));
+
     }
 
     public void navigateTo(String url,String  currentDriver) {
@@ -264,22 +268,16 @@ public class DriverManager {
                 case WEB:
                     if (currentDriver.equals("driver")) {
                         driver = new ChromeDriver();
-                        driver.clearPreferences();
-                        driver.setup();
                         driver.manage().window().maximize();
                         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
                         wait = new WebDriverWait(driver, 10 * 60);
                     }else if (currentDriver.equals("driver1")){
                         driver1 = new ChromeDriver();
-                        driver.clearPreferences();
-                        driver.setup();
                          driver1.manage().window().maximize();
                          driver1.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
                          wait = new WebDriverWait(driver1, 10 * 60);
                     }else if (currentDriver.equals("driver2")){
                         driver2 = new ChromeDriver();
-                        driver.clearPreferences();
-                        driver.setup();
                         driver2.manage().window().maximize();
                         driver2.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
                         wait = new WebDriverWait(driver2, 10 * 60);
